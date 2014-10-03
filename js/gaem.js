1c9f5dfba6082763e4c9189e63150403be7b1243
Adrián <idrian89@gmail.com>
2014-10-02 21:34:17 -0430

    Creando Mundo 1
    Prueba de personaje y mundo

--- js/gaem.js
+++ js/gaem.js
@@ -6,41 +6,43 @@ var cursors;
 function preload() {
 
     game.load.spritesheet('player','img/sprite.png',32,52);
-    game.load.tilemap('world','img/world.png',null, Phaser.Tilemap.PNG);
+    game.load.spritesheet('world','img/world.png',1920,600,1);
 
 }
 
 function create() {
 	cursor = game.input.keyboard.createCursorKeys();
-    
-	game.add.tileSprite(0, 0, 600, 1920, 'world');
+   
     game.world.setBounds(0, 0, 1920, 600);
 	game.stage.backgroundColor = '#124184';
     game.physics.startSystem(Phaser.Physics.NINJA);
 
-	 
-    player = game.add.sprite(0, 250, 'player', 0);
-    player.anchor.setTo(0.5, 0.5);
+	
+	
+    player = game.add.sprite(0, 50, 'player', 0);
+    player.anchor.setTo(0.5, 1);
 	game.physics.ninja.enableAABB(player);
     game.camera.follow(player);
 	
 	var bottom = game.world.height;
-	wWorld = game.add.sprite(0, bottom, 'world');
-	game.physics.ninja.enableTile(wWorld);
+	wWorld = game.add.sprite(0, 0, 'world');
+	game.physics.ninja.enableTile(wWorld,wWorld.frame);
 	wWorld.body.allowGravity = false;
 }
 
 function update() {
+	game.physics.ninja.collide(player, wWorld);
 
-	if (cursor.left.isDown)
-		player.body.velocity.x = -80;
-	else if (cursor.right.isDown)
-		player.body.velocity.x = 80;
-	else
-		player.body.velocity.x = 0;
+	if (cursor.left.isDown){
+		player.body.moveLeft(20);
+		}
+	else if (cursor.right.isDown){
+		player.body.moveRight(20);
+		console.log("asads");}
+
 		
-	if (cursor.up.isDown && player.body.touching.down)
-		player.body.velocity.y = -180;
+	if (cursor.up.isDown && player.body.touching.down){
+		player.body.moveUp(1300);}
 
 }
 
